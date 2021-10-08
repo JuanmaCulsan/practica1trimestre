6712d5023b781c19ba081ea3a95832f1d4dd3722
JuanmaCulsan <jcr0005@alu.medac.es>
2021-10-08 10:29:36 +0200

    dia 3 cambios en el logging y en conexionbbdd

--- conexionbbdd.php
+++ conexionbbdd.php
@@ -1,36 +1,70 @@
-<pre>
-    <?php
-        
-        $db_host = "localhost";
-        $db_name = "practica1php";
-        $db_user = "practica1php";
-        $db_pass = "edupablojuanma";
-        
-        $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
-        
-        if (mysqli_connect_error()) {
-            echo mysqli_connect_error();
-            exit;
-        }      
-        echo "Connected successfully.";
+<?php
 
-
-        $codigo = $_GET['name'];
-        
-        $sql = "SELECT *
-                FROM listausuario
-                where nombreuser = '$codigo'
-                ORDER BY coduser";
+    require("header.php");
     
-        $results = mysqli_query($conn, $sql);
-        
-
-        if ($sql) {
-            echo mysqli_error($conn);
-        } else {
-            $users = mysqli_fetch_all($results, MYSQLI_ASSOC);
+    $db_host = "localhost";
+    $db_name = "practica1php";
+    $db_user = "root";
+    $db_pass = "";
     
-            print_r($users);
-        }
-    ?>
-</pre>
+    $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+ 
+    if (mysqli_connect_error()) {
+        echo mysqli_connect_error();
+        exit;
+    }
+
+    $usuario = $_GET['nombre'];
+    
+    $pass = $_GET['pass'];
+ 
+    //TIENE QUE TENER EL MISMO VALOR QUE LA COLUMNA DE LA TABLA Y EL VALOR QUE QUEREMOS buscar
+    $sql = "SELECT * FROM usuario where id_usu = $usuario";
+ 
+    $results = mysqli_query($conn, $sql);
+    
+ 
+    if ($results === false) {
+        echo mysqli_error($conn);
+    } else {
+        $user = mysqli_fetch_all($results, MYSQLI_ASSOC);
+
+        print_r ($user);
+    }
+    
+?>
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>USUARIOS</h1>
+    </header>
+ 
+    <main>
+        <?php if (empty($user)): ?>
+            <p>No hay ningún usuario registrado</p>
+        <?php else: ?>
+            <ul>
+                <!--poner aqui el formulario-->
+                <form action="conexionbbdd.php" method="GET">
+                    <label for="">Nombre</label><br>
+                    <input type="text" value="<? $usuario['usuario'] ?>" id=""><br>
+                    <label for="">login</label><br>
+                    <input type="text" value="<? $usu['login'] ?>" id=""><br>
+                    <label for="">vehiculos</label><br>
+                    <input type="text" value="<? $usu['login'] ?>" id=""><br>
+                </form>
+            </ul>
+        <?php endif; ?>
+    </main>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body{
+            padding: 0;
+            margin: 0;
+        }
+        .header{
+            background-color: blue;
+            overflow: hidden;
+        }
+
+        .header h1{
+            margin-left: 2%;
+        }
+
+        .footer{
+            position: fixed;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+            background-color: #23bac4;
+            color: white;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <div class="header">
+        <h1>Bienvenido</h1>
+    </div>
+
+    <div class="footer">
+        <p>Todos los berberechos reservador</p>
+    </div>
+    
+</body>
+</html>
