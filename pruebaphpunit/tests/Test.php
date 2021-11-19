25a0e32d1d154100fd9ccade62e68bb6b0f36a9b
JuanmaCulsan <jcr0005@alu.medac.es>
2021-11-19 14:23:24 +0100

    prueba unitarias cedidas por Miguel Tapia

---
+++ pruebaphpunit/tests/Test.php
@@ -0,0 +1,17 @@
+<?php //declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+require("../practica1trimestre/funciones/cambiodatos_funciones.php");
+
+//use RegistrarUsuarios;
+
+class Test extends TestCase{
+
+    public function editar_usu("hola","adios","pass",5) : void{
+
+        $this->assertTrue(true);
+
+    }
+}
+
